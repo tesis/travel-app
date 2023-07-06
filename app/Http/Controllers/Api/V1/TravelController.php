338620e717e7ea8e30ee82a97567cd2497b124b5
stereza <tereza.simcic@gmail.com>
2023-07-06 07:49:50 +0200

    add http resource - number of nights will not return by default, thus add all fields that are needed, exclude timestamps, implement in controller

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -9,6 +9,8 @@ class TravelController extends Controller
 {
     public function index()
     {
-        return Travel::where('is_public', true)->get();
+        $travels = Travel::where('is_public', true)->get();
+
+        return TravelResource::collection($travels);
     }
 }
