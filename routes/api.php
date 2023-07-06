75e82c7ccb1808c1cfa11eac0dfb18e0e15cc7a1
stereza <tereza.simcic@gmail.com>
2023-07-06 08:10:28 +0200

    add getRouteKeyName for tours, add new endpoint for tour

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TravelController;
+use App\Http\Controllers\Api\V1\TourController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('travels', [TravelController::class, 'index']);
+// Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+Route::get('travels/{travel}/tours', [TourController::class, 'index']);
