a233fb42c36fb69b30c0a68f3309595520c6ab66
stereza <tereza.simcic@gmail.com>
2023-07-06 08:12:32 +0200

    add tour controller and resource

---
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\TourResource;
+use App\Models\Travel;
+
+class TourController extends Controller
+{
+    public function index(Travel $travel)
+    {
+        $tours = $travel->tours()
+            ->orderBy('starting_date')
+            ->paginate();
+
+        return TourResource::collection($tours);
+    }
+}
