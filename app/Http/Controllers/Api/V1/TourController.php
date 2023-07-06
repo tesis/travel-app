863809febda701043c57434c6b738e6c28c3cea5
stereza <tereza.simcic@gmail.com>
2023-07-06 08:33:27 +0200

    add tour list request for validation and implement it in controller

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -6,22 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
+use App\Http\Requests\ToursListRequest;
 
 class TourController extends Controller
 {
     /** Requirements: Users can filter (search) the results by priceFrom, priceTo, dateFrom (from that startingDate) and dateTo (until that startingDate).User can sort the list by price asc and desc. They will always be sorted, after every additional user-provided filter, by startingDate asc. */
     // /api/v1/travels/{slug}/tours?priceFrom=123&priceTo=456&dateFrom=2023-06-01&dateTo=2023-07-01
-    public function index(Travel $travel, Request $request)
+    public function index(Travel $travel, ToursListRequest $request)
     {
-        $request->validate([
-            'priceFrom' => 'numeric',
-            'priceTo' => 'numeric',
-            'dateFrom' => 'date',
-            'dateTo' => 'date',
-            'sortBy' => Rule::in(['price']),
-            'sortOrder' => Rule::in(['asc', 'desc']),
-        ]);
-
         $tours = $travel->tours()
             /*->when($request->priceFrom, function ($query) use ($request) {
                 $query->where('price', '>=', $request->priceFrom * 100);
