4209eee1f424b0e8cee9a941d25ba29c37492f9a
stereza <tereza.simcic@gmail.com>
2023-07-08 11:29:41 +0200

    create login request, add new endpoint - with sanctum middleware and add login method to Login controller

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -12,6 +15,18 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'error' => 'The provided credentials are incorrect.',
+            ], 422);
+        }
+
+        $device = substr($request->userAgent() ?? '', 0, 255);
+
+        return response()->json([
+            'access_token' => $user->createToken($device)->plainTextToken,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TravelController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\Admin\TravelController as AdminTravelController;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +25,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('travels', [TravelController::class, 'index']);
 // Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
 Route::get('travels/{travel}/tours', [TourController::class, 'index']);
-Route::prefix('admin')->group(function () {
+/*Route::prefix('admin')->group(function () {
     // Route::post('travels', [Admin\TravelController::class, 'store']);
     Route::post('travels', [AdminTravelController::class, 'store']);
+});*/
+// Use sanctum middleware to protect the endpoint
+Route::prefix('admin')->middleware(['auth:sanctum'])->group(function () {
+    Route::post('travels', [Admin\TravelController::class, 'store']);
 });
+
+Route::post('login', LoginController::class);
